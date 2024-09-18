837f5e260d100c1536ba7f7a71e5e4fb30ab2411
sterscrim <sterscrim@gmail.com>
2024-09-18 15:16:43 -0400

    fire screen shake

--- lab1 chessboard/chesboard/Assets/Scripts/Week Five/Player.cs
+++ lab1 chessboard/chesboard/Assets/Scripts/Week Five/Player.cs
@@ -1,9 +1,13 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CinemachineImpulseSource))]
 public class Player : MonoBehaviour
 {
+    private CinemachineImpulseSource cinemachineImpulse;
+
     public GameObject laserPrefab;
 
     private float horizontalScreenLimit = 10f;
@@ -20,12 +24,23 @@ public class Player : MonoBehaviour
     }
 
  
+    private void Start()
+    {
+        cinemachineImpulse = GetComponent<CinemachineImpulseSource>();
+    }
+    private void ApplyShootFireEffect()
+    {
+        cinemachineImpulse.GenerateImpulse();
+    }
 
-    void Shooting()
+
+
+public void Shooting()
     {
         if (Input.GetKeyDown(KeyCode.Space) && canShoot)
         {
             Instantiate(laserPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+            ApplyShootFireEffect();
             canShoot = false;
             StartCoroutine("Cooldown");
         }
